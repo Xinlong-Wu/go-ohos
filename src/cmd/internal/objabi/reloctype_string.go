suzhengda/go

支持musl的tls-gd模式，编译时需要指定-tlsmodegd

src/cmd/internal/objabi/reloctype_string.go
@@ -40,74 +40,76 @@ func _() {
 	_ = x[R_JMPMIPS-30]
 	_ = x[R_DWARFSECREF-31]
 	_ = x[R_DWARFFILEREF-32]
-	_ = x[R_ARM64_TLS_LE-33]
-	_ = x[R_ARM64_TLS_IE-34]
-	_ = x[R_ARM64_GOTPCREL-35]
-	_ = x[R_ARM64_GOT-36]
-	_ = x[R_ARM64_PCREL-37]
-	_ = x[R_ARM64_PCREL_LDST8-38]
-	_ = x[R_ARM64_PCREL_LDST16-39]
-	_ = x[R_ARM64_PCREL_LDST32-40]
-	_ = x[R_ARM64_PCREL_LDST64-41]
-	_ = x[R_ARM64_LDST8-42]
-	_ = x[R_ARM64_LDST16-43]
-	_ = x[R_ARM64_LDST32-44]
-	_ = x[R_ARM64_LDST64-45]
-	_ = x[R_ARM64_LDST128-46]
-	_ = x[R_POWER_TLS_LE-47]
-	_ = x[R_POWER_TLS_IE-48]
-	_ = x[R_POWER_TLS-49]
-	_ = x[R_POWER_TLS_IE_PCREL34-50]
-	_ = x[R_POWER_TLS_LE_TPREL34-51]
-	_ = x[R_ADDRPOWER_DS-52]
-	_ = x[R_ADDRPOWER_GOT-53]
-	_ = x[R_ADDRPOWER_GOT_PCREL34-54]
-	_ = x[R_ADDRPOWER_PCREL-55]
-	_ = x[R_ADDRPOWER_TOCREL-56]
-	_ = x[R_ADDRPOWER_TOCREL_DS-57]
-	_ = x[R_ADDRPOWER_D34-58]
-	_ = x[R_ADDRPOWER_PCREL34-59]
-	_ = x[R_RISCV_JAL-60]
-	_ = x[R_RISCV_JAL_TRAMP-61]
-	_ = x[R_RISCV_CALL-62]
-	_ = x[R_RISCV_PCREL_ITYPE-63]
-	_ = x[R_RISCV_PCREL_STYPE-64]
-	_ = x[R_RISCV_TLS_IE-65]
-	_ = x[R_RISCV_TLS_LE-66]
-	_ = x[R_RISCV_GOT_HI20-67]
-	_ = x[R_RISCV_PCREL_HI20-68]
-	_ = x[R_RISCV_PCREL_LO12_I-69]
-	_ = x[R_RISCV_PCREL_LO12_S-70]
-	_ = x[R_RISCV_BRANCH-71]
-	_ = x[R_RISCV_RVC_BRANCH-72]
-	_ = x[R_RISCV_RVC_JUMP-73]
-	_ = x[R_PCRELDBL-74]
-	_ = x[R_LOONG64_ADDR_HI-75]
-	_ = x[R_LOONG64_ADDR_LO-76]
-	_ = x[R_LOONG64_TLS_LE_HI-77]
-	_ = x[R_LOONG64_TLS_LE_LO-78]
-	_ = x[R_CALLLOONG64-79]
-	_ = x[R_LOONG64_TLS_IE_HI-80]
-	_ = x[R_LOONG64_TLS_IE_LO-81]
-	_ = x[R_LOONG64_GOT_HI-82]
-	_ = x[R_LOONG64_GOT_LO-83]
-	_ = x[R_LOONG64_ADD64-84]
-	_ = x[R_LOONG64_SUB64-85]
-	_ = x[R_JMP16LOONG64-86]
-	_ = x[R_JMP21LOONG64-87]
-	_ = x[R_JMPLOONG64-88]
-	_ = x[R_ADDRMIPSU-89]
-	_ = x[R_ADDRMIPSTLS-90]
-	_ = x[R_ADDRCUOFF-91]
-	_ = x[R_WASMIMPORT-92]
-	_ = x[R_XCOFFREF-93]
-	_ = x[R_PEIMAGEOFF-94]
-	_ = x[R_INITORDER-95]
+	_ = x[R_ARM64_TLS_GD-33]
+	_ = x[R_ARM64_TLS_GD_PAGEOFF12-34]
+	_ = x[R_ARM64_TLS_LE-35]
+	_ = x[R_ARM64_TLS_IE-36]
+	_ = x[R_ARM64_GOTPCREL-37]
+	_ = x[R_ARM64_GOT-38]
+	_ = x[R_ARM64_PCREL-39]
+	_ = x[R_ARM64_PCREL_LDST8-40]
+	_ = x[R_ARM64_PCREL_LDST16-41]
+	_ = x[R_ARM64_PCREL_LDST32-42]
+	_ = x[R_ARM64_PCREL_LDST64-43]
+	_ = x[R_ARM64_LDST8-44]
+	_ = x[R_ARM64_LDST16-45]
+	_ = x[R_ARM64_LDST32-46]
+	_ = x[R_ARM64_LDST64-47]
+	_ = x[R_ARM64_LDST128-48]
+	_ = x[R_POWER_TLS_LE-49]
+	_ = x[R_POWER_TLS_IE-50]
+	_ = x[R_POWER_TLS-51]
+	_ = x[R_POWER_TLS_IE_PCREL34-52]
+	_ = x[R_POWER_TLS_LE_TPREL34-53]
+	_ = x[R_ADDRPOWER_DS-54]
+	_ = x[R_ADDRPOWER_GOT-55]
+	_ = x[R_ADDRPOWER_GOT_PCREL34-56]
+	_ = x[R_ADDRPOWER_PCREL-57]
+	_ = x[R_ADDRPOWER_TOCREL-58]
+	_ = x[R_ADDRPOWER_TOCREL_DS-59]
+	_ = x[R_ADDRPOWER_D34-60]
+	_ = x[R_ADDRPOWER_PCREL34-61]
+	_ = x[R_RISCV_JAL-62]
+	_ = x[R_RISCV_JAL_TRAMP-63]
+	_ = x[R_RISCV_CALL-64]
+	_ = x[R_RISCV_PCREL_ITYPE-65]
+	_ = x[R_RISCV_PCREL_STYPE-66]
+	_ = x[R_RISCV_TLS_IE-67]
+	_ = x[R_RISCV_TLS_LE-68]
+	_ = x[R_RISCV_GOT_HI20-69]
+	_ = x[R_RISCV_PCREL_HI20-70]
+	_ = x[R_RISCV_PCREL_LO12_I-71]
+	_ = x[R_RISCV_PCREL_LO12_S-72]
+	_ = x[R_RISCV_BRANCH-73]
+	_ = x[R_RISCV_RVC_BRANCH-74]
+	_ = x[R_RISCV_RVC_JUMP-75]
+	_ = x[R_PCRELDBL-76]
+	_ = x[R_LOONG64_ADDR_HI-77]
+	_ = x[R_LOONG64_ADDR_LO-78]
+	_ = x[R_LOONG64_TLS_LE_HI-79]
+	_ = x[R_LOONG64_TLS_LE_LO-80]
+	_ = x[R_CALLLOONG64-81]
+	_ = x[R_LOONG64_TLS_IE_HI-82]
+	_ = x[R_LOONG64_TLS_IE_LO-83]
+	_ = x[R_LOONG64_GOT_HI-84]
+	_ = x[R_LOONG64_GOT_LO-85]
+	_ = x[R_LOONG64_ADD64-86]
+	_ = x[R_LOONG64_SUB64-87]
+	_ = x[R_JMP16LOONG64-88]
+	_ = x[R_JMP21LOONG64-89]
+	_ = x[R_JMPLOONG64-90]
+	_ = x[R_ADDRMIPSU-91]
+	_ = x[R_ADDRMIPSTLS-92]
+	_ = x[R_ADDRCUOFF-93]
+	_ = x[R_WASMIMPORT-94]
+	_ = x[R_XCOFFREF-95]
+	_ = x[R_PEIMAGEOFF-96]
+	_ = x[R_INITORDER-97]
 }
 
-const _RelocType_name = "R_ADDRR_ADDRPOWERR_ADDRARM64R_ADDRMIPSR_ADDROFFR_SIZER_CALLR_CALLARMR_CALLARM64R_CALLINDR_CALLPOWERR_CALLMIPSR_CONSTR_PCRELR_TLS_LER_TLS_IER_GOTOFFR_PLT0R_PLT1R_PLT2R_USEFIELDR_USETYPER_USEIFACER_USEIFACEMETHODR_USENAMEDMETHODR_METHODOFFR_KEEPR_POWER_TOCR_GOTPCRELR_JMPMIPSR_DWARFSECREFR_DWARFFILEREFR_ARM64_TLS_LER_ARM64_TLS_IER_ARM64_GOTPCRELR_ARM64_GOTR_ARM64_PCRELR_ARM64_PCREL_LDST8R_ARM64_PCREL_LDST16R_ARM64_PCREL_LDST32R_ARM64_PCREL_LDST64R_ARM64_LDST8R_ARM64_LDST16R_ARM64_LDST32R_ARM64_LDST64R_ARM64_LDST128R_POWER_TLS_LER_POWER_TLS_IER_POWER_TLSR_POWER_TLS_IE_PCREL34R_POWER_TLS_LE_TPREL34R_ADDRPOWER_DSR_ADDRPOWER_GOTR_ADDRPOWER_GOT_PCREL34R_ADDRPOWER_PCRELR_ADDRPOWER_TOCRELR_ADDRPOWER_TOCREL_DSR_ADDRPOWER_D34R_ADDRPOWER_PCREL34R_RISCV_JALR_RISCV_JAL_TRAMPR_RISCV_CALLR_RISCV_PCREL_ITYPER_RISCV_PCREL_STYPER_RISCV_TLS_IER_RISCV_TLS_LER_RISCV_GOT_HI20R_RISCV_PCREL_HI20R_RISCV_PCREL_LO12_IR_RISCV_PCREL_LO12_SR_RISCV_BRANCHR_RISCV_RVC_BRANCHR_RISCV_RVC_JUMPR_PCRELDBLR_LOONG64_ADDR_HIR_LOONG64_ADDR_LOR_LOONG64_TLS_LE_HIR_LOONG64_TLS_LE_LOR_CALLLOONG64R_LOONG64_TLS_IE_HIR_LOONG64_TLS_IE_LOR_LOONG64_GOT_HIR_LOONG64_GOT_LOR_LOONG64_ADD64R_LOONG64_SUB64R_JMP16LOONG64R_JMP21LOONG64R_JMPLOONG64R_ADDRMIPSUR_ADDRMIPSTLSR_ADDRCUOFFR_WASMIMPORTR_XCOFFREFR_PEIMAGEOFFR_INITORDER"
+const _RelocType_name = "R_ADDRR_ADDRPOWERR_ADDRARM64R_ADDRMIPSR_ADDROFFR_SIZER_CALLR_CALLARMR_CALLARM64R_CALLINDR_CALLPOWERR_CALLMIPSR_CONSTR_PCRELR_TLS_LER_TLS_IER_GOTOFFR_PLT0R_PLT1R_PLT2R_USEFIELDR_USETYPER_USEIFACER_USEIFACEMETHODR_USENAMEDMETHODR_METHODOFFR_KEEPR_POWER_TOCR_GOTPCRELR_JMPMIPSR_DWARFSECREFR_DWARFFILEREFR_ARM64_TLS_GDR_ARM64_TLS_GD_PAGEOFF12R_ARM64_TLS_LER_ARM64_TLS_IER_ARM64_GOTPCRELR_ARM64_GOTR_ARM64_PCRELR_ARM64_PCREL_LDST8R_ARM64_PCREL_LDST16R_ARM64_PCREL_LDST32R_ARM64_PCREL_LDST64R_ARM64_LDST8R_ARM64_LDST16R_ARM64_LDST32R_ARM64_LDST64R_ARM64_LDST128R_POWER_TLS_LER_POWER_TLS_IER_POWER_TLSR_POWER_TLS_IE_PCREL34R_POWER_TLS_LE_TPREL34R_ADDRPOWER_DSR_ADDRPOWER_GOTR_ADDRPOWER_GOT_PCREL34R_ADDRPOWER_PCRELR_ADDRPOWER_TOCRELR_ADDRPOWER_TOCREL_DSR_ADDRPOWER_D34R_ADDRPOWER_PCREL34R_RISCV_JALR_RISCV_JAL_TRAMPR_RISCV_CALLR_RISCV_PCREL_ITYPER_RISCV_PCREL_STYPER_RISCV_TLS_IER_RISCV_TLS_LER_RISCV_GOT_HI20R_RISCV_PCREL_HI20R_RISCV_PCREL_LO12_IR_RISCV_PCREL_LO12_SR_RISCV_BRANCHR_RISCV_RVC_BRANCHR_RISCV_RVC_JUMPR_PCRELDBLR_LOONG64_ADDR_HIR_LOONG64_ADDR_LOR_LOONG64_TLS_LE_HIR_LOONG64_TLS_LE_LOR_CALLLOONG64R_LOONG64_TLS_IE_HIR_LOONG64_TLS_IE_LOR_LOONG64_GOT_HIR_LOONG64_GOT_LOR_LOONG64_ADD64R_LOONG64_SUB64R_JMP16LOONG64R_JMP21LOONG64R_JMPLOONG64R_ADDRMIPSUR_ADDRMIPSTLSR_ADDRCUOFFR_WASMIMPORTR_XCOFFREFR_PEIMAGEOFFR_INITORDER"
 
-var _RelocType_index = [...]uint16{0, 6, 17, 28, 38, 47, 53, 59, 68, 79, 88, 99, 109, 116, 123, 131, 139, 147, 153, 159, 165, 175, 184, 194, 210, 226, 237, 243, 254, 264, 273, 286, 300, 314, 328, 344, 355, 368, 387, 407, 427, 447, 460, 474, 488, 502, 517, 531, 545, 556, 578, 600, 614, 629, 652, 669, 687, 708, 723, 742, 753, 770, 782, 801, 820, 834, 848, 864, 882, 902, 922, 936, 954, 970, 980, 997, 1014, 1033, 1052, 1065, 1084, 1103, 1119, 1135, 1150, 1165, 1179, 1193, 1205, 1216, 1229, 1240, 1252, 1262, 1274, 1285}
+var _RelocType_index = [...]uint16{0, 6, 17, 28, 38, 47, 53, 59, 68, 79, 88, 99, 109, 116, 123, 131, 139, 147, 153, 159, 165, 175, 184, 194, 210, 226, 237, 243, 254, 264, 273, 286, 300, 314, 338, 352, 366, 382, 393, 406, 425, 445, 465, 485, 498, 512, 526, 540, 555, 569, 583, 594, 616, 638, 652, 667, 690, 707, 725, 746, 761, 780, 791, 808, 820, 839, 858, 872, 886, 902, 920, 940, 960, 974, 992, 1008, 1018, 1035, 1052, 1071, 1090, 1103, 1122, 1141, 1157, 1173, 1188, 1203, 1217, 1231, 1243, 1254, 1267, 1278, 1290, 1300, 1312, 1323}
 
 func (i RelocType) String() string {
 	i -= 1

Xinlong-Wu/go-ohos

src/cmd/internal/objabi/reloctype_string.go
@@ -1,4 +1,4 @@
-// Code generated by "stringer -type=RelocType"; DO NOT EDIT.
+// Code generated by "stringer -type=RelocType -output=reloctype_string.go reloctype.go"; DO NOT EDIT.
 
 package objabi
 
@@ -39,88 +39,90 @@ func _() {
 	_ = x[R_GOTPCREL-29]
 	_ = x[R_JMPMIPS-30]
 	_ = x[R_DWARFSECREF-31]
-	_ = x[R_ARM64_TLS_LE-32]
-	_ = x[R_ARM64_TLS_IE-33]
-	_ = x[R_ARM64_GOTPCREL-34]
-	_ = x[R_ARM64_GOT-35]
-	_ = x[R_ARM64_PCREL-36]
-	_ = x[R_ARM64_PCREL_LDST8-37]
-	_ = x[R_ARM64_PCREL_LDST16-38]
-	_ = x[R_ARM64_PCREL_LDST32-39]
-	_ = x[R_ARM64_PCREL_LDST64-40]
-	_ = x[R_ARM64_LDST8-41]
-	_ = x[R_ARM64_LDST16-42]
-	_ = x[R_ARM64_LDST32-43]
-	_ = x[R_ARM64_LDST64-44]
-	_ = x[R_ARM64_LDST128-45]
-	_ = x[R_POWER_TLS_LE-46]
-	_ = x[R_POWER_TLS_IE-47]
-	_ = x[R_POWER_TLS-48]
-	_ = x[R_POWER_TLS_IE_PCREL34-49]
-	_ = x[R_POWER_TLS_LE_TPREL34-50]
-	_ = x[R_ADDRPOWER_DS-51]
-	_ = x[R_ADDRPOWER_GOT-52]
-	_ = x[R_ADDRPOWER_GOT_PCREL34-53]
-	_ = x[R_ADDRPOWER_PCREL-54]
-	_ = x[R_ADDRPOWER_TOCREL-55]
-	_ = x[R_ADDRPOWER_TOCREL_DS-56]
-	_ = x[R_ADDRPOWER_D34-57]
-	_ = x[R_ADDRPOWER_PCREL34-58]
-	_ = x[R_RISCV_JAL-59]
-	_ = x[R_RISCV_JAL_TRAMP-60]
-	_ = x[R_RISCV_CALL-61]
-	_ = x[R_RISCV_PCREL_ITYPE-62]
-	_ = x[R_RISCV_PCREL_STYPE-63]
-	_ = x[R_RISCV_TLS_IE-64]
-	_ = x[R_RISCV_TLS_LE-65]
-	_ = x[R_RISCV_GOT_HI20-66]
-	_ = x[R_RISCV_GOT_PCREL_ITYPE-67]
-	_ = x[R_RISCV_PCREL_HI20-68]
-	_ = x[R_RISCV_PCREL_LO12_I-69]
-	_ = x[R_RISCV_PCREL_LO12_S-70]
-	_ = x[R_RISCV_BRANCH-71]
-	_ = x[R_RISCV_ADD32-72]
-	_ = x[R_RISCV_SUB32-73]
-	_ = x[R_RISCV_RVC_BRANCH-74]
-	_ = x[R_RISCV_RVC_JUMP-75]
-	_ = x[R_PCRELDBL-76]
-	_ = x[R_LOONG64_ADDR_HI-77]
-	_ = x[R_LOONG64_ADDR_LO-78]
-	_ = x[R_LOONG64_ADDR_PCREL20_S2-79]
-	_ = x[R_LOONG64_TLS_LE_HI-80]
-	_ = x[R_LOONG64_TLS_LE_LO-81]
-	_ = x[R_CALLLOONG64-82]
-	_ = x[R_LOONG64_CALL36-83]
-	_ = x[R_LOONG64_TLS_IE_HI-84]
-	_ = x[R_LOONG64_TLS_IE_LO-85]
-	_ = x[R_LOONG64_GOT_HI-86]
-	_ = x[R_LOONG64_GOT_LO-87]
-	_ = x[R_LOONG64_ADD64-88]
-	_ = x[R_LOONG64_SUB64-89]
-	_ = x[R_JMP16LOONG64-90]
-	_ = x[R_JMP21LOONG64-91]
-	_ = x[R_JMPLOONG64-92]
-	_ = x[R_ADDRMIPSU-93]
-	_ = x[R_ADDRMIPSTLS-94]
-	_ = x[R_ADDRCUOFF-95]
-	_ = x[R_WASMIMPORT-96]
-	_ = x[R_XCOFFREF-97]
-	_ = x[R_PEIMAGEOFF-98]
-	_ = x[R_INITORDER-99]
-	_ = x[R_DWTXTADDR_U1-100]
-	_ = x[R_DWTXTADDR_U2-101]
-	_ = x[R_DWTXTADDR_U3-102]
-	_ = x[R_DWTXTADDR_U4-103]
+	_ = x[R_ARM64_TLS_GD-32]
+	_ = x[R_ARM64_TLS_GD_PAGEOFF12-33]
+	_ = x[R_ARM64_TLS_LE-34]
+	_ = x[R_ARM64_TLS_IE-35]
+	_ = x[R_ARM64_GOTPCREL-36]
+	_ = x[R_ARM64_GOT-37]
+	_ = x[R_ARM64_PCREL-38]
+	_ = x[R_ARM64_PCREL_LDST8-39]
+	_ = x[R_ARM64_PCREL_LDST16-40]
+	_ = x[R_ARM64_PCREL_LDST32-41]
+	_ = x[R_ARM64_PCREL_LDST64-42]
+	_ = x[R_ARM64_LDST8-43]
+	_ = x[R_ARM64_LDST16-44]
+	_ = x[R_ARM64_LDST32-45]
+	_ = x[R_ARM64_LDST64-46]
+	_ = x[R_ARM64_LDST128-47]
+	_ = x[R_POWER_TLS_LE-48]
+	_ = x[R_POWER_TLS_IE-49]
+	_ = x[R_POWER_TLS-50]
+	_ = x[R_POWER_TLS_IE_PCREL34-51]
+	_ = x[R_POWER_TLS_LE_TPREL34-52]
+	_ = x[R_ADDRPOWER_DS-53]
+	_ = x[R_ADDRPOWER_GOT-54]
+	_ = x[R_ADDRPOWER_GOT_PCREL34-55]
+	_ = x[R_ADDRPOWER_PCREL-56]
+	_ = x[R_ADDRPOWER_TOCREL-57]
+	_ = x[R_ADDRPOWER_TOCREL_DS-58]
+	_ = x[R_ADDRPOWER_D34-59]
+	_ = x[R_ADDRPOWER_PCREL34-60]
+	_ = x[R_RISCV_JAL-61]
+	_ = x[R_RISCV_JAL_TRAMP-62]
+	_ = x[R_RISCV_CALL-63]
+	_ = x[R_RISCV_PCREL_ITYPE-64]
+	_ = x[R_RISCV_PCREL_STYPE-65]
+	_ = x[R_RISCV_TLS_IE-66]
+	_ = x[R_RISCV_TLS_LE-67]
+	_ = x[R_RISCV_GOT_HI20-68]
+	_ = x[R_RISCV_GOT_PCREL_ITYPE-69]
+	_ = x[R_RISCV_PCREL_HI20-70]
+	_ = x[R_RISCV_PCREL_LO12_I-71]
+	_ = x[R_RISCV_PCREL_LO12_S-72]
+	_ = x[R_RISCV_BRANCH-73]
+	_ = x[R_RISCV_ADD32-74]
+	_ = x[R_RISCV_SUB32-75]
+	_ = x[R_RISCV_RVC_BRANCH-76]
+	_ = x[R_RISCV_RVC_JUMP-77]
+	_ = x[R_PCRELDBL-78]
+	_ = x[R_LOONG64_ADDR_HI-79]
+	_ = x[R_LOONG64_ADDR_LO-80]
+	_ = x[R_LOONG64_ADDR_PCREL20_S2-81]
+	_ = x[R_LOONG64_TLS_LE_HI-82]
+	_ = x[R_LOONG64_TLS_LE_LO-83]
+	_ = x[R_CALLLOONG64-84]
+	_ = x[R_LOONG64_CALL36-85]
+	_ = x[R_LOONG64_TLS_IE_HI-86]
+	_ = x[R_LOONG64_TLS_IE_LO-87]
+	_ = x[R_LOONG64_GOT_HI-88]
+	_ = x[R_LOONG64_GOT_LO-89]
+	_ = x[R_LOONG64_ADD64-90]
+	_ = x[R_LOONG64_SUB64-91]
+	_ = x[R_JMP16LOONG64-92]
+	_ = x[R_JMP21LOONG64-93]
+	_ = x[R_JMPLOONG64-94]
+	_ = x[R_ADDRMIPSU-95]
+	_ = x[R_ADDRMIPSTLS-96]
+	_ = x[R_ADDRCUOFF-97]
+	_ = x[R_WASMIMPORT-98]
+	_ = x[R_XCOFFREF-99]
+	_ = x[R_PEIMAGEOFF-100]
+	_ = x[R_INITORDER-101]
+	_ = x[R_DWTXTADDR_U1-102]
+	_ = x[R_DWTXTADDR_U2-103]
+	_ = x[R_DWTXTADDR_U3-104]
+	_ = x[R_DWTXTADDR_U4-105]
 }
 
-const _RelocType_name = "R_ADDRR_ADDRPOWERR_ADDRARM64R_ADDRMIPSR_ADDROFFR_SIZER_CALLR_CALLARMR_CALLARM64R_CALLINDR_CALLPOWERR_CALLMIPSR_CONSTR_PCRELR_TLS_LER_TLS_IER_GOTOFFR_PLT0R_PLT1R_PLT2R_USEFIELDR_USETYPER_USEIFACER_USEIFACEMETHODR_USENAMEDMETHODR_METHODOFFR_KEEPR_POWER_TOCR_GOTPCRELR_JMPMIPSR_DWARFSECREFR_ARM64_TLS_LER_ARM64_TLS_IER_ARM64_GOTPCRELR_ARM64_GOTR_ARM64_PCRELR_ARM64_PCREL_LDST8R_ARM64_PCREL_LDST16R_ARM64_PCREL_LDST32R_ARM64_PCREL_LDST64R_ARM64_LDST8R_ARM64_LDST16R_ARM64_LDST32R_ARM64_LDST64R_ARM64_LDST128R_POWER_TLS_LER_POWER_TLS_IER_POWER_TLSR_POWER_TLS_IE_PCREL34R_POWER_TLS_LE_TPREL34R_ADDRPOWER_DSR_ADDRPOWER_GOTR_ADDRPOWER_GOT_PCREL34R_ADDRPOWER_PCRELR_ADDRPOWER_TOCRELR_ADDRPOWER_TOCREL_DSR_ADDRPOWER_D34R_ADDRPOWER_PCREL34R_RISCV_JALR_RISCV_JAL_TRAMPR_RISCV_CALLR_RISCV_PCREL_ITYPER_RISCV_PCREL_STYPER_RISCV_TLS_IER_RISCV_TLS_LER_RISCV_GOT_HI20R_RISCV_GOT_PCREL_ITYPER_RISCV_PCREL_HI20R_RISCV_PCREL_LO12_IR_RISCV_PCREL_LO12_SR_RISCV_BRANCHR_RISCV_ADD32R_RISCV_SUB32R_RISCV_RVC_BRANCHR_RISCV_RVC_JUMPR_PCRELDBLR_LOONG64_ADDR_HIR_LOONG64_ADDR_LOR_LOONG64_ADDR_PCREL20_S2R_LOONG64_TLS_LE_HIR_LOONG64_TLS_LE_LOR_CALLLOONG64R_LOONG64_CALL36R_LOONG64_TLS_IE_HIR_LOONG64_TLS_IE_LOR_LOONG64_GOT_HIR_LOONG64_GOT_LOR_LOONG64_ADD64R_LOONG64_SUB64R_JMP16LOONG64R_JMP21LOONG64R_JMPLOONG64R_ADDRMIPSUR_ADDRMIPSTLSR_ADDRCUOFFR_WASMIMPORTR_XCOFFREFR_PEIMAGEOFFR_INITORDERR_DWTXTADDR_U1R_DWTXTADDR_U2R_DWTXTADDR_U3R_DWTXTADDR_U4"
+const _RelocType_name = "R_ADDRR_ADDRPOWERR_ADDRARM64R_ADDRMIPSR_ADDROFFR_SIZER_CALLR_CALLARMR_CALLARM64R_CALLINDR_CALLPOWERR_CALLMIPSR_CONSTR_PCRELR_TLS_LER_TLS_IER_GOTOFFR_PLT0R_PLT1R_PLT2R_USEFIELDR_USETYPER_USEIFACER_USEIFACEMETHODR_USENAMEDMETHODR_METHODOFFR_KEEPR_POWER_TOCR_GOTPCRELR_JMPMIPSR_DWARFSECREFR_ARM64_TLS_GDR_ARM64_TLS_GD_PAGEOFF12R_ARM64_TLS_LER_ARM64_TLS_IER_ARM64_GOTPCRELR_ARM64_GOTR_ARM64_PCRELR_ARM64_PCREL_LDST8R_ARM64_PCREL_LDST16R_ARM64_PCREL_LDST32R_ARM64_PCREL_LDST64R_ARM64_LDST8R_ARM64_LDST16R_ARM64_LDST32R_ARM64_LDST64R_ARM64_LDST128R_POWER_TLS_LER_POWER_TLS_IER_POWER_TLSR_POWER_TLS_IE_PCREL34R_POWER_TLS_LE_TPREL34R_ADDRPOWER_DSR_ADDRPOWER_GOTR_ADDRPOWER_GOT_PCREL34R_ADDRPOWER_PCRELR_ADDRPOWER_TOCRELR_ADDRPOWER_TOCREL_DSR_ADDRPOWER_D34R_ADDRPOWER_PCREL34R_RISCV_JALR_RISCV_JAL_TRAMPR_RISCV_CALLR_RISCV_PCREL_ITYPER_RISCV_PCREL_STYPER_RISCV_TLS_IER_RISCV_TLS_LER_RISCV_GOT_HI20R_RISCV_GOT_PCREL_ITYPER_RISCV_PCREL_HI20R_RISCV_PCREL_LO12_IR_RISCV_PCREL_LO12_SR_RISCV_BRANCHR_RISCV_ADD32R_RISCV_SUB32R_RISCV_RVC_BRANCHR_RISCV_RVC_JUMPR_PCRELDBLR_LOONG64_ADDR_HIR_LOONG64_ADDR_LOR_LOONG64_ADDR_PCREL20_S2R_LOONG64_TLS_LE_HIR_LOONG64_TLS_LE_LOR_CALLLOONG64R_LOONG64_CALL36R_LOONG64_TLS_IE_HIR_LOONG64_TLS_IE_LOR_LOONG64_GOT_HIR_LOONG64_GOT_LOR_LOONG64_ADD64R_LOONG64_SUB64R_JMP16LOONG64R_JMP21LOONG64R_JMPLOONG64R_ADDRMIPSUR_ADDRMIPSTLSR_ADDRCUOFFR_WASMIMPORTR_XCOFFREFR_PEIMAGEOFFR_INITORDERR_DWTXTADDR_U1R_DWTXTADDR_U2R_DWTXTADDR_U3R_DWTXTADDR_U4"
 
-var _RelocType_index = [...]uint16{0, 6, 17, 28, 38, 47, 53, 59, 68, 79, 88, 99, 109, 116, 123, 131, 139, 147, 153, 159, 165, 175, 184, 194, 210, 226, 237, 243, 254, 264, 273, 286, 300, 314, 330, 341, 354, 373, 393, 413, 433, 446, 460, 474, 488, 503, 517, 531, 542, 564, 586, 600, 615, 638, 655, 673, 694, 709, 728, 739, 756, 768, 787, 806, 820, 834, 850, 873, 891, 911, 931, 945, 958, 971, 989, 1005, 1015, 1032, 1049, 1074, 1093, 1112, 1125, 1141, 1160, 1179, 1195, 1211, 1226, 1241, 1255, 1269, 1281, 1292, 1305, 1316, 1328, 1338, 1350, 1361, 1375, 1389, 1403, 1417}
+var _RelocType_index = [...]uint16{0, 6, 17, 28, 38, 47, 53, 59, 68, 79, 88, 99, 109, 116, 123, 131, 139, 147, 153, 159, 165, 175, 184, 194, 210, 226, 237, 243, 254, 264, 273, 286, 300, 324, 338, 352, 368, 379, 392, 411, 431, 451, 471, 484, 498, 512, 526, 541, 555, 569, 580, 602, 624, 638, 653, 676, 693, 711, 732, 747, 766, 777, 794, 806, 825, 844, 858, 872, 888, 911, 929, 949, 969, 983, 996, 1009, 1027, 1043, 1053, 1070, 1087, 1112, 1131, 1150, 1163, 1179, 1198, 1217, 1233, 1249, 1264, 1279, 1293, 1307, 1319, 1330, 1343, 1354, 1366, 1376, 1388, 1399, 1413, 1427, 1441, 1455}
 
 func (i RelocType) String() string {
-	i -= 1
-	if i < 0 || i >= RelocType(len(_RelocType_index)-1) {
-		return "RelocType(" + strconv.FormatInt(int64(i+1), 10) + ")"
+	idx := int(i) - 1
+	if i < 1 || idx >= len(_RelocType_index)-1 {
+		return "RelocType(" + strconv.FormatInt(int64(i), 10) + ")"
 	}
-	return _RelocType_name[_RelocType_index[i]:_RelocType_index[i+1]]
+	return _RelocType_name[_RelocType_index[idx]:_RelocType_index[idx+1]]
 }